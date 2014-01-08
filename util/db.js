1fd068caf3ff2e7a7bb5e174ea61e4eb1ab3906f
Davidson Fellipe <email@fellipe.com>
2014-01-08 17:57:52 -0200

    changing label - Connecting to MongoDB

--- util/db.js
+++ util/db.js
@@ -5,7 +5,7 @@ var Server = mongo.Server,
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = new Db('wpomonitordb', server, { safe: true });
 db.open(function(err, db) {
-    console.log('Connecting to MonboDB: %s', db.databaseName);
+    console.log('Connecting to MongoDB: %s', db.databaseName);
     if (err) {
         console.trace(err);
         process.exit(1);
