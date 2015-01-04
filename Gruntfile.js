387dfe4d1b5f1d32ada7221e327d222296c7aa87
Davidson Fellipe <email@fellipe.com>
2015-01-04 02:28:51 -0200

    [grunt] adding Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  var path = require('path');
+
+  // measures the time each task takes
+  // require('time-grunt')(grunt);
+
+  // load-grunt-config includes load-grunt-tasks
+  require('load-grunt-config')(grunt, {
+    configPath: path.join(process.cwd(), 'scripts/grunt'),
+    data: { //data passed into config.  Can use with <%= test %>
+      path: {
+        src: 'src/',
+        dest: 'build/'
+      }
+    }
+  });
+};
